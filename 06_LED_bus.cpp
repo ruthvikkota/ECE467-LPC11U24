c3b26ef659212cb07629d44b756f9b8426698275
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-08-07 12:44:00 +0530

    Update 06_LED_bus.cpp

--- 06_LED_bus.cpp
+++ 06_LED_bus.cpp
@@ -11,3 +11,18 @@ int main() {
       }
   }
 }
+/* For 4 internal LEDs
+#include "mbed.h"
+
+BusOut myled(LED4,LED3,LED2,LED1);
+
+int main() {
+    unsigned char i;
+  while(1) {
+      for(i=0;i<=15;i++){
+          myled.write(i);
+          wait(1);
+      }
+  }
+}
+*/
