63b9ecad1bdfec2336092b79200bc669e03d9bcd
pujith8055 <68635356+pujith8055@users.noreply.github.com>
2023-08-18 10:23:39 +0530

    Update 11_seven_segment_1.cpp
    extended from 0 and 1 to all 9 digits

--- 11_seven_segment_1.cpp
+++ 11_seven_segment_1.cpp
@@ -1,3 +1,4 @@
+//seven segment display with common cathode normal method
 #include "mbed.h"
 
 DigitalOut a(p5);
@@ -6,30 +7,120 @@ DigitalOut c(p7);
 DigitalOut d(p8);
 DigitalOut e(p9);
 DigitalOut f(p10);
-DigitalOut segg(p11);
+DigitalOut g(p11);
 
-int main() {
-  while(1) {
-      //For zero
-    a=0;
-    b=0;
-    c=0;
-    d=0;
-    e=0;
-    f=0;
-    g=1;
+int main()
+{
+    while(1)
+    {
+        //0
+        a=1;
+        b=1;
+        c=1;
+        d=1;
+        e=1;
+        f=1;
+        g=0;
 
-    wait(0.6);
+        wait(1);
 
-      //For one
-    a=1;
-    b=0;
-    c=0;
-    d=1;
-    e=1;
-    f=1;
-    g=1;
+        //1
+        a=0;
+        b=1;
+        c=1;
+        d=0;
+        e=0;
+        f=0;
+        g=0;
 
-    wait(0.6);
-  }
+        wait(1);
+
+        //2
+        a=1;
+        b=1;
+        c=0;
+        d=1;
+        e=1;
+        f=0;
+        g=1;
+
+        wait(1);
+
+        //3
+        a=1;
+        b=1;
+        c=1;
+        d=1;
+        e=0;
+        f=0;
+        g=1;
+
+        wait(1);
+
+        //4
+        a=0;
+        b=1;
+        c=1;
+        d=0;
+        e=0;
+        f=1;
+        g=1;
+
+        wait(1);
+
+        //5
+        a=1;
+        b=0;
+        c=1;
+        d=1;
+        e=0;
+        f=1;
+        g=1;
+
+        wait(1);
+
+        //6
+        a=1;
+        b=0;
+        c=1;
+        d=1;
+        e=1;
+        f=1;
+        g=1;
+
+        wait(1);
+
+        //7
+        a=1;
+        b=1;
+        c=1;
+        d=0;
+        e=0;
+        f=0;
+        g=1;
+
+        wait(1);
+
+        //8
+        a=1;
+        b=1;
+        c=1;
+        d=1;
+        e=1;
+        f=1;
+        g=1;
+
+        wait(1);
+
+        //9
+        a=1;
+        b=1;
+        c=1;
+        d=1;
+        e=0;
+        f=1;
+        g=1;
+
+        wait(1);
+    }
 }
