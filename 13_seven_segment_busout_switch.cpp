d7909781ca3760ffe32151ef9e11a16d2daf2a45
pujith8055 <68635356+pujith8055@users.noreply.github.com>
2023-08-18 11:22:14 +0530

    Update 13_seven_segment_busout_switch.cpp
    one press one number

--- 13_seven_segment_busout_switch.cpp
+++ 13_seven_segment_busout_switch.cpp
@@ -8,13 +8,17 @@ int main()
 {
     unsigned char i = 0;
     unsigned char display[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+    bool button_pressed = false;
 
     while(1){
-    if(button)
-    {
-        i=(i+1)%10;
+        if (button.read() == 1 && !button_pressed) {
+            button_pressed = true;
+            i=(i+1)%10;
+        } 
+        else if (button.read() == 0) {
+            button_pressed = false;
+        }
         seg.write(display[i]);
-        wait(1);
-    }
+        wait(0.5);
     }
 }
